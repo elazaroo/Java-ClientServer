c94ad8485297570d5cc05fe1a488e5931c75835b
elazaroo <enekolazaroalba@gmail.com>
2024-10-23 08:15:22 +0200

    Documented code

--- src/client/Client.java
+++ src/client/Client.java
@@ -12,12 +12,17 @@ public class Client {
 			Socket skClient = new Socket(HOST, PORT);
 			OutputStream aux = skClient.getOutputStream();
 			DataOutputStream flow = new DataOutputStream(aux);
+			
+			// Send as many numbers as you want
 			flow.writeDouble(10);
 			flow.writeDouble(5);
 			flow.writeDouble(10);
+			
+			// Recieve servers response
 			InputStream iAux = skClient.getInputStream();
 			DataInputStream iFlow = new DataInputStream(iAux);
 			System.out.println("The AVG is " + iFlow.readDouble());
+			iFlow.close();
 			flow.close();
 			skClient.close();
 		} catch (Exception e) {
--- src/server/Server.java
+++ src/server/Server.java
@@ -12,26 +12,39 @@ public class Server {
 		try {
 			ServerSocket skServer = new ServerSocket(PORT);
 			System.out.println("Listening port: " + PORT);
+			
+			// Abort when maxClients is arrived
 			for (int clientNum = 0; clientNum < maxClients; clientNum++) {
-				Socket skClient = skServer.accept(); // Create the object
 				System.out.println("Serving client " + clientNum);
+				
+				Socket skClient = skServer.accept();				
 				InputStream aux = skClient.getInputStream();
 				DataInputStream flow = new DataInputStream(aux);
+				
+				// Declare ArrayList numbers where doubles will be stored
 				List<Double> numbers = new ArrayList<Double>();
 				do {
 					numbers.add(flow.readDouble());
 				} while (flow.available() > 0);
 				System.out.println(numbers.size() + " numbers recieved.");
+				
+				// Use for (each number of ArrayList) and sum in a variable
 				double sum = 0;
 				for (double num : numbers) {
 					sum += num;
 				}
+				
 				double avg = sum / numbers.size();
 				System.out.println("AVG: " + avg);
+				
+				// At least one number in ArrayList
 				if (numbers.size() > 0) {
+					
+					// Send response to client
 					OutputStream oAux = skClient.getOutputStream();
 					DataOutputStream oFlow = new DataOutputStream(oAux);
 					oFlow.writeDouble(avg);
+					oFlow.close();
 				}
 				flow.close();
 				skClient.close();
